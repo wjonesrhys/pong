4fedb254fd89ae3dad7bc34eaa75cb2e738f3726
Rhys Jones <wjones.rhys@gmail.com>
2023-11-10 22:21:00 +0000

    Revert "cherry-pick test"
    This reverts commit c131954e193775497905f4489b60684176afe0e7.

--- headers/gamestate.hpp
+++ headers/gamestate.hpp
@@ -1,32 +1,5 @@
 #pragma once
 #include <SFML/Graphics.hpp>
-<<<<<<< HEAD
-#include <state.hpp>
-
-#include <iostream>
-#include <vector>
-
-class GameState : public State {
-public:
-    GameState() {
-        setStateName("State B");
-    }
-
-    void enter() {
-        print("i");
-        // Additional actions when entering State B
-    }
-
-    void exit() override {
-        State::exit();
-        // Additional actions when exiting State B
-    }
-
-    void update() override {
-        State::update();
-        // Additional actions when updating State B
-    }
-=======
 #include <iostream>
 #include <vector>
 
@@ -36,5 +9,4 @@ class GameState {
     public:
         void update();
 
->>>>>>> a6da76c (fix issues?)
 };
